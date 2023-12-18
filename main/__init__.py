95be2bbec6f6e74bb0bd3c43eaef3d764c41d8a0
Wesley Smith <yelsew414@gmail.com>
2023-12-18 11:58:47 -0500

    added steal stage form and code to randomly select who stole from player if there is more than one person who selected to steal from that player.

--- main/__init__.py
+++ main/__init__.py
@@ -1,5 +1,5 @@
 from otree.api import *
-
+import random
 
 doc = """
 Your app description
@@ -10,6 +10,8 @@ class C(BaseConstants):
     NAME_IN_URL = 'main'
     PLAYERS_PER_GROUP = 2
     NUM_ROUNDS = 15
+    
+    INCOME = 5
 
 
 class Subsession(BaseSubsession):
@@ -22,6 +24,10 @@ class Group(BaseGroup):
 
 class Player(BasePlayer):
     work_completed = models.IntegerField(initial=0)
+    earnings = models.IntegerField(initial=0)
+    selected_to_steal = models.IntegerField()
+    steal_from = models.IntegerField()
+    report = models.BooleanField()
 
 
 # PAGES
@@ -37,6 +43,7 @@ class WorkingStage(Page):
         print('received a bid from', player.id_in_group, ':', data)
         if data == True:
             player.work_completed += 1
+            player.earnings += C.INCOME
 
 
 class WorkingStageWait(WaitPage):
@@ -44,11 +51,30 @@ class WorkingStageWait(WaitPage):
 
 
 class StealingStage(Page):
-    pass
+    form_model = 'player'
+    form_fields = ['selected_to_steal']
 
 
 class StealingStageWait(WaitPage):
-    pass
+    @staticmethod
+    def after_all_players_arrive(group: Group):
+        for player in group.get_players():
+            print(player.id_in_group)
+            players = [p for p in group.get_players() if p.selected_to_steal == player.id_in_group]
+            
+            if len(players) > 1:
+                # randomly select who to steal from
+                rand_player = random.choose(players)
+                print(f"The selected player to steal from {player.id_in_group} is {rand_player.id_in_group}.")
+                pass
+            elif len(players) == 1:
+                # assign final steal
+                print(f"Only {players[0].id_in_group} selected to steal from {player.id_in_group}.")
+                pass
+            else:
+                # player chose not to steal
+                print(f"Nobody stole from {player.id_in_group}")
+                continue
 
 
 class ReportingStage(Page):
