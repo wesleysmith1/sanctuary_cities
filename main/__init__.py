ec7e0ada80aaa881347e122428d8ef4c0dafa6c9
Wesley Smith <yelsew414@gmail.com>
2023-12-18 22:20:20 -0500

    tested code. stealer is selected if there are multiple. reporting front end template added and

--- main/__init__.py
+++ main/__init__.py
@@ -25,9 +25,9 @@ class Group(BaseGroup):
 class Player(BasePlayer):
     work_completed = models.IntegerField(initial=0)
     earnings = models.IntegerField(initial=0)
-    selected_to_steal = models.IntegerField()
+    selected_to_steal = models.IntegerField(blank=True)
     steal_from = models.IntegerField()
-    report = models.BooleanField()
+    report = models.BooleanField(label="Would you like to report the player that stole from you?")
 
 
 # PAGES
@@ -60,15 +60,17 @@ class StealingStageWait(WaitPage):
     def after_all_players_arrive(group: Group):
         for player in group.get_players():
             print(player.id_in_group)
-            players = [p for p in group.get_players() if p.selected_to_steal == player.id_in_group]
-            
+            players = [p for p in player.get_others_in_group() if p.field_maybe_none('selected_to_steal') == player.id_in_group]
+                        
             if len(players) > 1:
                 # randomly select who to steal from
                 rand_player = random.choose(players)
+                rand_player.steal_from = player.id_in_group
                 print(f"The selected player to steal from {player.id_in_group} is {rand_player.id_in_group}.")
                 pass
             elif len(players) == 1:
                 # assign final steal
+                players[0].steal_from = player.id_in_group
                 print(f"Only {players[0].id_in_group} selected to steal from {player.id_in_group}.")
                 pass
             else:
@@ -76,9 +78,25 @@ class StealingStageWait(WaitPage):
                 print(f"Nobody stole from {player.id_in_group}")
                 continue
 
+def get_stealing_player(player):
+    for p in player.get_others_in_group():
+        if p.steal_from == player.id_in_group:
+            return p.id_in_group
+    return None
 
 class ReportingStage(Page):
-    pass
+    form_model = "player"
+    form_fields = ["report"]
+    
+    @staticmethod
+    def is_displayed(player: Player):
+        return len([p for p in player.get_others_in_group() if p.field_maybe_none('steal_from') == player.id_in_group]) > 0
+    
+    @staticmethod
+    def vars_for_template(player: Player):
+        return dict(
+            stealing_player=get_stealing_player(player),
+        )
 
 
 page_sequence = [RoundStartWait, WorkingStage, WorkingStageWait, StealingStage, StealingStageWait, ReportingStage]
